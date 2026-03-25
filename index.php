15a4a4bc13681ce6b2da79c6ff635e421fa60a07
Hackk00 <jesuspolo00@gmail.com>
2026-03-24 23:20:02 -0500

    Multi Chat

---
+++ index.php
@@ -0,0 +1,15 @@
+<style>
+	body {
+		margin: 0;
+		padding: 15px;
+		box-sizing: border-box;
+		background: #1F1F1F;
+	}
+</style>
+
+<iframe
+src="https://botrix.live/widgets/chat/?bid=tDDNdegeMIRyvfOLz78NZw&bots=false&emojis=true&hideCommands=false&hideMessages=false&hideMessagesSeconds=10&widgetSize=17&trovo=false&twitch=true&youtube=true&kick=true&platformIcon=true&theme=bubbles&ignoredUsers=&streamTogether=true&cheer=true&pointsReward=false&animation=fadeIn&showTimestamp=false&shadowThickness=1&autoScroll=true"
+width="100%"
+height="100%"
+style="border:none;">
+</iframe>
